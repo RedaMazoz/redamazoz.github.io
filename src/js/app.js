3c9fef97d39b32b5ac3bcde5af2d90e50a1a5f3c
Reda Mazoz <69761147+Tensadev@users.noreply.github.com>
2021-04-11 08:43:42 +0000

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "value": "#ffffff"
       },
       "shape": {
-        "type": "polygon",
+        "type": "circle",
         "stroke": {
           "width": 0,
           "color": "#000000"
